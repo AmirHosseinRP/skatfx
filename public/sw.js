5c16b6278773de111fe75bac28e976dc5c9ceb71
Amirhossein Rezapanah <a.rezapanah@dotin.ir>
2025-12-15 16:04:47 +0330

    feat: install and activate added to sw

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,21 @@
 const cacheName = "v1";
 
-const cacheClone = async e => {
-  const res = await fetch(e.request);
-  const resClone = res.clone();
+// Install event - activate immediately
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
 
-  const cache = await caches.open(cacheName);
-  await cache.put(e.request, resClone);
-  return res;
-};
+// Activate event - clean up old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames => {
+        return Promise.all(cacheNames.filter(name => name !== cacheName).map(name => caches.delete(name)));
+      })
+      .then(() => self.clients.claim())
+  );
+});
 
 self.addEventListener("fetch", event => {
   const url = new URL(event.request.url);
@@ -26,3 +34,12 @@ self.addEventListener("fetch", event => {
       .then(res => res)
   );
 });
+
+const cacheClone = async e => {
+  const res = await fetch(e.request);
+  const resClone = res.clone();
+
+  const cache = await caches.open(cacheName);
+  await cache.put(e.request, resClone);
+  return res;
+};
